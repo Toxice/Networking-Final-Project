471075b3f1f7a6647e83407fdbcb4cb9557e0b96
Nick <nickisenpai18@gmail.com>
2026-02-23 14:32:42 +0200

    dns is done.

--- DNSQuestion.py
+++ DNSQuestion.py
@@ -6,18 +6,40 @@ class DNSQuestion:
     def parse_question(self,start_index):
         #qname
         labels  = []
+        found_terminator = False
+        total_length = 0
         i = start_index
-        while self.raw_data[i] != 0:
+            #checking bounds
+        if i >= len(self.raw_data):
+            raise ValueError("QNAME out of bounds. malformed data")
+            #maximum length of dns label
+        while i < len(self.raw_data):
             length = self.raw_data[i]
-            labels.append(self.raw_data[i+1:i + 1 + length].decode())
+            if length == 0:
+                found_terminator = True
+                i = i + 1
+                break
+            if length > 63:
+                raise ValueError("label is too long. trying hack me?")
+            if i + 1 + length > len(self.raw_data):
+                raise ValueError("buffer overrun. malformed data")
+            total_length += length + 1
+            if total_length > 255:
+                raise ValueError("maximum length is 255. malformed data")
+            labels.append(self.raw_data[i + 1 : i + 1 + length].decode())
             i = i + 1 + length
-        next_offset = i + 1
+        if not found_terminator:
+            raise ValueError("missing zero terminator in QNAME malformed data")
+            #checking whether we have at least 4 bytes left
+        next_offset = i
+        if next_offset + 4 > len(self.raw_data):
+            raise ValueError("no bytes for qtype and qclass. malformed data")
         #qtype
         qtype = int.from_bytes(self.raw_data[next_offset:next_offset + 2],byteorder='big')
         qclass = int.from_bytes(self.raw_data[next_offset+2:next_offset + 4],byteorder='big')
         next_offset = next_offset + 4
         return ".".join(labels),qtype,qclass,next_offset
-"""
+    """
 if __name__ == "__main__":
     raw_packet = (
         b'\x00' * 12 +               # fake 12-byte header
--- DNSServer.py
+++ DNSServer.py
@@ -1,54 +1,75 @@
 from xml.etree.ElementInclude import include
 
 from DNSResponseBuilder import DNSResponseBuilder
+import struct
 
 class DNSServer:
     def __init__(self, zone_database):
         self.zone_database = zone_database
+
+
     def handle_request(self, data):
-        builder = DNSResponseBuilder(data)
-        builder.parse_request()
-
-        qname = builder.qname
-        qtype = builder.qtype
-
-        # 1. Authority check
-        #REFUSED -- not in out database
-        # we dont work with google.com
-        if not self.zone_database.is_in_zone(qname):
-            include_soa=False
-            rcode = 5
-            aa = 0 #authoritive answer
-            ip = None
-        #in the database
-        else:
-            #for any name inside our zone
-            # 2. Type check (only support A) ipv4
-            #we dont support AAAA ipv6
-            if qtype != 1:
-                include_soa = True
-                rcode = 0
-                aa = 1
-                ip = None
-
+        #working with bad input
+        if data is None:
+            return b''
+        if len(data) < 12:
+            if len(data) < 2:
+                return b''
             else:
-                # we got example.com
-                ip = self.zone_database.lookup(qname)
+                transaction_id = int.from_bytes(data[0:2],byteorder='big')
+                flags = (1 << 15) | 1  # QR=1, RCODE=1
+                return struct.pack("!HHHHHH", transaction_id, flags, 0, 0, 0, 0)
 
-                #in our zone. NOERROR
-                if ip is not None:
-                    include_soa = False
+
+        try:
+            builder = DNSResponseBuilder(data)
+            builder.parse_request()
+
+            qname = builder.qname
+            qtype = builder.qtype
+
+            # 1. Authority check
+            #REFUSED -- not in out database
+            # we dont work with google.com
+            if not self.zone_database.is_in_zone(qname):
+                include_soa=False
+                rcode = 5
+                aa = 0 #authoritive answer
+                ip = None
+            #in the database
+            else:
+                #for any name inside our zone
+                # 2. Type check (only support A) ipv4
+                #we dont support AAAA ipv6
+                if qtype != 1:
+                    include_soa = True
                     rcode = 0
                     aa = 1
-                #NXDOMAIN
+                    ip = None
+
                 else:
-                    include_soa = True
-                    rcode = 3
-                    aa = 1
+                    # we got example.com
+                    ip = self.zone_database.lookup(qname)
 
-        if include_soa:
-            soa_data = self.zone_database.get_soa()
-        else:
-            soa_data = None
+                    #in our zone. NOERROR
+                    if ip is not None:
+                        include_soa = False
+                        rcode = 0
+                        aa = 1
+                    #NXDOMAIN
+                    else:
+                        include_soa = True
+                        rcode = 3
+                        aa = 1
 
-        return builder.build_response(aa, rcode, self.zone_database.get_name(),include_soa, ip, soa_data)
+            if include_soa:
+                soa_data = self.zone_database.get_soa()
+            else:
+                soa_data = None
+
+            return builder.build_response(aa, rcode, self.zone_database.get_name(),include_soa, ip, soa_data)
+        except ValueError:
+            transaction_id = int.from_bytes(data[0:2], byteorder='big')
+            flags = (1 << 15) | 1  # QR=1, RCODE=1
+            return struct.pack("!HHHHHH", transaction_id, flags, 0, 0, 0, 0)
+
