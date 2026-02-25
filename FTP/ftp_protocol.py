667f10f2dd96e8092b393ec1c512ed2fa9f3a0f8
Mor <toxice290@gmail.com>
2026-02-25 12:19:29 +0200

    FTP server resposnes at ftp_protocol.py

---
+++ FTP/ftp_protocol.py
@@ -0,0 +1,17 @@
+# ftp_protocol.py
+
+def service_ready(self):
+    return {"response": "service ready"}
+
+def file_okay(byte_size):
+    return {"response": "file okay",
+            "byte_size": byte_size}
+
+def closing_data():
+    return {"response": "closing data"}
+
+def service_teardown():
+    return {"response": "service teardown"}
+
+def file_unavailable():
+    return {"response": "file unavailable"}
