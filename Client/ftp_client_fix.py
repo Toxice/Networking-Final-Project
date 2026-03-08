601521e3d887012eae1a34e5661d01e0c16360f3
Mor <toxice290@gmail.com>
2026-03-08 08:24:11 +0200

    Fixed the BUG in transferring a file in RUDP

--- Client/ftp_client_fix.py
+++ Client/ftp_client_fix.py
@@ -201,11 +201,7 @@ class FTPClient:
             try:
                 while True:
                     data, addr = client_sock.recvfrom(BUFFER_SIZE)
-                    if data == b"DONE":
-                        if len(received_packets) == total_expected and total_expected > 0:
-                            break
-                        else:
-                            continue
+                    if data == b"DONE": break
 
                     if len(data) >= 8:
                         p_num, total_expected = struct.unpack("!II", data[:8])
